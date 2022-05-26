bc95c339f05fa74faca35b6fa9f1d95ec5f4212d
zafar83 <zafar.kust@gmail.com>
2022-05-27 00:46:34 +0500

    api/paypal/createorder

---
+++ GemCare.API/PayPal.Business/PayPalService.cs
@@ -0,0 +1,22 @@
+﻿using GemCare.API.PayPal.Business.Request;
+using GemCare.API.PayPal.Business.Response;
+using System.Threading.Tasks;
+
+namespace GemCare.API.PayPal.Business
+{
+    public class PayPalService : IPayPalService
+    {
+        private CreateOrderResponse _createOrderResponse;
+        private CreateOrderResponse _CreateOrderResponse(CreateOrderRequest obj)
+        {
+            Task.Run(() => PayPalRestClient.CreateOrder(obj)).Wait();
+            this._createOrderResponse= PayPalRestClient.createOrderResponse;
+            return _createOrderResponse;
+        }
+        public CreateOrderResponse CreateOrder(CreateOrderRequest createOrderObject)
+        {
+            _CreateOrderResponse(createOrderObject);
+            return this._createOrderResponse;
+        }
+    }
+}
